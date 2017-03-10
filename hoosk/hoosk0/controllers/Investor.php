a5b10c1cdd263ddc0f2e67466fabdb0ac8dab5a9
Ahsan Khan <ahsandev.creative>
2017-03-10 18:15:29 +0500

    New Changes

--- hoosk/hoosk0/controllers/Investor.php
+++ hoosk/hoosk0/controllers/Investor.php
@@ -55,8 +55,30 @@ class Investor extends MY_Controller {
         $resultsFor = $this->input->post('resultsFor');
         $keyword    = $this->input->post('keyword');
 
+        $filterStartDate  = $this->input->post('filterStartDate');
+        $filterEndDate    = $this->input->post('filterEndDate');
+
+        $statusFilter     = $this->input->post('statusFilter');
+
         $selectData = '*';
-        $where = 'email LIKE "%' .$keyword.'%" OR company_name LIKE "%' .$keyword.'%" OR firstName LIKE "%' .$keyword.'%" OR lastName LIKE "%' .$keyword.'%"';
+        $where = '';
+        if(!empty($keyword)){
+            $where = 'email LIKE "%' .$keyword.'%" OR company_name LIKE "%' .$keyword.'%" OR firstName LIKE "%' .$keyword.'%" OR lastName LIKE "%' .$keyword.'%"';
+        }
+
+        if(!empty($filterStartDate) && !empty($filterEndDate) ){
+            $where = '( email LIKE "%' .$keyword.'%" OR company_name LIKE "%' .$keyword.'%" OR firstName LIKE "%' .$keyword.'%" OR lastName LIKE "%' .$keyword.'%" )';
+            $where .=' AND ( added_date BETWEEN "'.$filterStartDate.'" AND "'.$filterEndDate.'" )';
+        }
+        if(!empty($statusFilter)){
+            if(!empty($keyword) || !empty($filterStartDate)){
+                $where .=  ' AND';
+            }
+             $where .=  'status = '.$statusFilter.' ';
+        }
+        
+
+        
         $joins = array(
             array(
                 'table'     => 'esic_investor EI',
@@ -71,7 +93,11 @@ class Investor extends MY_Controller {
         );
 
         $data['list'] = $this->Common_model->select_fields_where_like_join('hoosk_user',$selectData,$joins,$where);
-        $data['Title'] = 'Investors';
+        $data['Query'] = $this->db->last_query();
+        $data['filterStartDate']    =  $filterStartDate;
+        $data['filterEndDate']      =  $filterEndDate;
+        $data['keyword']            =  $keyword;
+
         $this->load->view('theme/header');
         $this->load->view('theme/listing',$data);
         $this->load->view('theme/footer');
--- hoosk/hoosk0/views/theme/listing.php
+++ hoosk/hoosk0/views/theme/listing.php
@@ -1,17 +1,52 @@
+<?php
+if(isset($keyword)){
+    $keyword = $keyword;
+}else{
+    $keyword = '';
+}
 
+
+?>
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+<link rel="stylesheet" type="text/css"  href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-daterangepicker/2.1.24/daterangepicker.min.css"/>
+<script type="text/javascript"  src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+<script type="text/javascript"  src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-daterangepicker/2.1.24/moment.min.js"></script>
+<script type="text/javascript"  src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-daterangepicker/2.1.24/daterangepicker.min.js"></script>
 <div class="container" style="margin-top: 90px;">
 	<h1>Search Results For <?php if(isset($Title)){ echo $Title; } ?></h1>
 	<div class="search-filter-container">
-	</div>
+		<div class="filter-heading">
+          	<h3>Filter By:</h3>
+        </div>
+        <div class="range-filters">
+        	<label>Added Date</label>
+            <form>
+	            <input type="text" name="value_from_start_date"  class="form-control" data-datepicker="separateRange" placeholder="Start" value="<?= $filterStartDate; ?>"/>
+	            <span><strong>To</strong></span>
+	            <input type="text" name="value_from_end_date"  class="form-control" data-datepicker="separateRange" placeholder="End"  value="<?= $filterEndDate; ?>"/>
+            </form>
+            <label>Status</label>
+            <form>
+	          <select id="statusFilter" name="status" class="form-control" >
+	            <option value="">-- Select --</option>
+	          	<option value="0">Pending</option>
+	          	<option value="1">Publish</option>
+	          </select>
+            </form>
+        </div>
+    </div>
 	<div class="search-results-container">
 		<div class="header_search item-container clearfix">
-			<form action="results_investors" method="post" accept-charset="utf-8">
-	            <input type="text" name="keyword" class="form-control" placeholder="Investors">
+			<form id="searchForm" action="results_investors" method="post" accept-charset="utf-8">
+	            <input type="text" name="keyword" class="form-control" placeholder="Investors" value="<?= $keyword ?>">
 	            <input type="hidden" name="resultsFor" value="Investors">
-	             <input type="submit" value="" class="form-control submit-icon" >
+	            <input type="hidden" id="filterStartDate" name="filterStartDate" value="">
+	            <input type="hidden" id="filterEndDate"   name="filterEndDate" 	value="">
+	            <input type="hidden" id="statusFilterField" name="statusFilter" value="">
+	            <input type="button" value="" class="form-control submit-icon" >
 	        </form>
         </div>
-
+	<?php if(isset($Query)){ echo $Query; } ?>
 	<?php 
 	  if(isset($list) && !empty($list)){
 	  	  foreach ($list as $key => $item) { 
@@ -76,5 +111,83 @@
 	  		}
 	  	} 
 	?>
+
 	 </div>
 </div>
+
+
+<script>
+    var separator = ' - ', dateFormat = 'YYYY-MM-DD';
+    var options = {
+        autoUpdateInput: false,
+        locale: {
+            format: dateFormat,
+            separator: separator
+        },
+
+        opens: "right"
+    };
+
+
+    $('[data-datepicker=separateRange]')
+        .daterangepicker(options)
+        .on('apply.daterangepicker' ,function(ev, picker) {
+            var boolStart = this.name.match(/value_from_start_/g) == null ? false : true;
+            var boolEnd = this.name.match(/value_from_end_/g) == null ? false : true;
+
+            var mainName = this.name.replace('value_from_start_', '');
+            if(boolEnd) {
+                mainName = this.name.replace('value_from_end_', '');
+                $(this).closest('form').find('[name=value_from_end_'+ mainName +']').blur();
+            }
+
+            $(this).closest('form').find('[name=value_from_start_'+ mainName +']').val(picker.startDate.format(dateFormat));
+            $(this).closest('form').find('[name=value_from_end_'+ mainName +']').val(picker.endDate.format(dateFormat));
+
+            $(this).trigger('change').trigger('keyup');
+
+            $('#filterStartDate').val(picker.startDate.format(dateFormat));
+            $('#filterEndDate').val(picker.endDate.format(dateFormat));
+            var statusFilter = $('#statusFilter').val();
+            $('#statusFilterField').val(statusFilter);
+            $('#searchForm').submit();
+
+
+        })
+        .on('show.daterangepicker', function(ev, picker) {
+            var boolStart = this.name.match(/value_from_start_/g) == null ? false : true;
+            var boolEnd = this.name.match(/value_from_end_/g) == null ? false : true;
+            var mainName = this.name.replace('value_from_start_', '');
+            if(boolEnd) {
+                mainName = this.name.replace('value_from_end_', '');
+            }
+
+            var startDate = $(this).closest('form').find('[name=value_from_start_'+ mainName +']').val();
+            var endDate = $(this).closest('form').find('[name=value_from_end_'+ mainName +']').val();
+
+            $('[name=daterangepicker_start]').val(startDate).trigger('change').trigger('keyup');
+            $('[name=daterangepicker_end]').val(endDate).trigger('change').trigger('keyup');
+
+            if(boolEnd) {
+                $('[name=daterangepicker_end]').focus();
+            }
+        });
+
+        $('.submit-icon').click(function(event) {
+        	var statusFilter = $('#statusFilter').val();
+        	$('#filterStartDate').val($('input[name="value_from_start_date"]').val());
+            $('#filterEndDate').val($('input[name="value_from_end_date"]').val());
+             $('#statusFilterField').val(statusFilter);
+            $('#searchForm').submit();
+        });
+
+        $('#statusFilter').change(function(e){
+        	var statusFilter = $(this).val();
+        	$('#filterStartDate').val($('input[name="value_from_start_date"]').val());
+            $('#filterEndDate').val($('input[name="value_from_end_date"]').val());
+            $('#statusFilterField').val(statusFilter);
+            $('#searchForm').submit();
+        });
+        
+
+</script>
