228459ce205a99bbb9eacee04f1bb8370ed9e606
Ahsan Khan <ahsandev.creative>
2017-03-13 16:56:41 +0500

    autoload

--- theme/dark/templates/footer.php
+++ theme/dark/templates/footer.php
@@ -84,13 +84,7 @@
 
 </div>-->
 <script src="<?php echo ADMIN_THEME; ?>/js/jquery-1.10.2.min.js"></script>
-
-
-<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
 <link href="<?=base_url();?>assets/css/filter.css" rel="stylesheet">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="<?=base_url();?>assets/js/filter.js"></script>
 
 
